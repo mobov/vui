f6cb46b370ef2a2aad0967a5401e8b5bc9951504
nocoolyoyo <574857444@qq.com>
2018-12-16 10:31:47 +0800

    clean

--- packages/megmore-ui/src/components.ts
+++
@@ -1,7 +0,0 @@
-// 基础
-export { default as MApp } from './app'
-export { default as MIcon } from './icon'
-// 拓展
-export { default as MAppBar } from './app-bar'
-export { default as MButton } from './button'
-export { default as MAvatar } from './avatar'
