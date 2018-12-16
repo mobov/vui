fec6c998fbd48ddc10e5afc0db5452e2369b7899
nocoolyoyo <574857444@qq.com>
2018-12-16 23:56:09 +0800

    update

--- src/lib/radio/index.ts
+++ src/lib/radio/index.ts
@@ -2,8 +2,8 @@ import './radio.scss'
 import MRadio from './radio'
 
 /* istanbul ignore next */
-MRadio.install = function install(Vue) {
-    Vue.component(MRadio.name, MRadio)
+MRadio.install = (Vue) => {
+  Vue.component(MRadio.name, MRadio)
 }
 
 export default MRadio
