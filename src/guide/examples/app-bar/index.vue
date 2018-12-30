98479c0f83a13a92640905fc6516279842f7106b
nocoolyoyo <574857444@qq.com>
2018-12-31 01:30:56 +0800

    update

---
+++ src/guide/examples/app-bar/index.vue
@@ -0,0 +1,40 @@
+<style lang="scss">
+  .example-view {
+    width: 100%;
+    height: 500px;
+  }
+</style>
+<template>
+  <div class="example-app-bar">
+    <div>
+      <m-app-bar :color="color" v-for="color in COLORS">
+        <m-button shape="circle" variety="flat" :elevation="0" icon="menu" />
+      </m-app-bar>
+    </div>
+    <div>
+      <m-app-bar :color="color" variety="flat" v-for="color in COLORS">
+        <m-button shape="circle" variety="flat" :color="color" :elevation="0" icon="menu" />
+      </m-app-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Megmore, { MAppBar } from '@/lib'
+
+  const { COLORS } = Megmore.constant
+
+  export default {
+    name: 'ExampleAppBar',
+    components: { MAppBar },
+    data () {
+      return {
+        COLORS
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+
+</style>
